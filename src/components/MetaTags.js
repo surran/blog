70761796319d5b3b7e195645a69d56615ece89a9
surya <suryaran@gmail.com>
2019-12-02 21:20:07 +0530

    Metatags file

---
+++ src/components/MetaTags.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import Helmet from "react-helmet";
+
+function MetaTags(props) {
+	const {title, description, type, imageUrl} = props
+	return (<Helmet>
+				<title>{title}</title>
+				<meta name="description" content={description} />
+    			<meta property="og:type" content={type} />
+    			<meta property="og:image" content={imageUrl} />
+    			<meta property="og:title" content={title} />
+ 				<meta property="og:description" content={description} />
+    		</Helmet>)
+}
+
+export default MetaTags
